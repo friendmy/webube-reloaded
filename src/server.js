dc1ab0c6375fc943469947c3a4544a91bcb83b98
friendmy <friendmytrue@gmail.com>
2021-06-04 07:29:39 +0900

    request response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", (req, res) => console.log(req, res));
+
 const handleListening = () => console.log(`✔ Server listening on port http://localhost:${PORT}`);
 
 app.listen(4000, handleListening);
