cf43fdaa234061224a4bc6ce37337769072df2dd
friendmy <friendmytrue@gmail.com>
2021-06-15 13:03:18 +0900

    #6.28

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,7 @@ console.log("finished")
 */
 
 export const home = async (req, res) => {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -42,7 +42,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title,
         description,
-        hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+        hashtags: Video.formatHashtags(hashtags),
     });
 
     return res.redirect(`/videos/${id}`);
@@ -58,11 +58,27 @@ export const postUpload = async (req, res) => {
         title,
         description,
         createdAt: Date.now(),
-        hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
-        meta: {
-            views: 0,
-            rating: 0,
-        },
+        hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
+};
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(`${keyword}$`, "i"),
+            },
+        });
+        console.log(videos);
+    }
+    return res.render("search", { pageTitle: "Search", videos });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -8,5 +8,7 @@ const handelEdit = (req, res) => res.send("Edit Video");
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 export default videoRouter;
