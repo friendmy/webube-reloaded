3fc133b95f1bbf5d448ac83b98df4909ec1e24fb
friendmy <friendmytrue@gmail.com>
2021-06-16 09:16:04 +0900

    User Create

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,29 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+    //console.log(req.body);
+    const { name, username, email, password, location } = req.body;
+    const pageTitle = "Join";
+    const usernameExists = await User.exists({ $or: [{ username }, { email }] });
+    //await User.create({ ...req.body });
+    if (usernameExists) {
+        return res.render("join", {
+            pageTitle: pageTitle,
+            errorMessage: "This username is already taken.",
+        });
+    }
+
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+
+    res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
