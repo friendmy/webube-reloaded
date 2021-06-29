a97cf2dee4386b8a7227d088a80500245dcae1cb
friendmy <friendmytrue@gmail.com>
2021-06-29 13:02:08 +0900

    #8.7 Upload Part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -207,7 +207,9 @@ export const postEdit = async (req, res) => {
             user: { _id, username: beforeUsername, email: beforeEmail },
         },
         body: { name, email, username, location },
+        file,
     } = req;
+    console.log(file);
     const pageTitle = "Edit Profile";
 
     const searchParam = [];
