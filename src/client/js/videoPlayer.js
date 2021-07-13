bdc667caddf9e355d40f2afd8ff3f89805e66527
friendmy <friendmytrue@gmail.com>
2021-07-14 08:59:21 +0900

    #11.6 Timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,9 +1,13 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
-let beforeVolume = 0;
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
     // if hte video is playing, pause it
@@ -19,14 +23,47 @@ const handleMute = (e) => {
     if (video.muted) {
         video.muted = false;
     } else {
-        beforeVolume = volumeRange.value;
+        volumeValue = volumeRange.value;
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : beforeVolume;
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (e) => {
+    const {
+        target: { value },
+    } = e;
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volumeValue = value;
+};
+
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const handleLoadedMetadata = (e) => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = (e) => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimeLineChange = (e) => {
+    const {
+        target: { value },
+    } = e;
+    video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-
-console.log(play, mute, time, volume);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeLineChange);
